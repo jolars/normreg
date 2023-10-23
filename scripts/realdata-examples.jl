a75adf3d6d83f837fbbd919d29c2a40097027865
Johan Larsson <johanlarsson@outlook.com>
2023-10-23 12:18:36 +0200

    fix(sims): rename and fix real data examples

---
+++ scripts/realdata-examples.jl
@@ -0,0 +1,18 @@
+using DrWatson
+@quickactivate "normreg"
+
+include(srcdir("compare_standardizations.jl"))
+
+using LIBSVMdata, Lasso, Statistics, Plots
+
+x, y = load_dataset("housing", dense = true, replace = false, verbose = true)
+compare_standardizations(x, y)
+
+x, y = load_dataset("triazines", dense = true, replace = false, verbose = true)
+compare_standardizations(x, y)
+
+x, y = load_dataset("leukemia", dense = true, replace = false, verbose = true)
+
+y[y.==-1] .= 0
+
+compare_standardizations(x, y, dist = GLM.Binomial())
