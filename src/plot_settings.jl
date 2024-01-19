9e35d9bb166690a1df55744eade89c56491948be
Johan Larsson <johanlarsson@outlook.com>
2024-01-19 19:30:54 +0100

    chore: lower title font size

--- src/plot_settings.jl
+++ src/plot_settings.jl
@@ -4,12 +4,10 @@ using LaTeXStrings
 using PythonPlot: matplotlib
 
 function setPlotSettings()
-  default(titlefontsize = 12, thickness_scaling = 1)
+  default(titlefontsize = 11, thickness_scaling = 1)
 
   pythonplot()
   # gr()
-
-  # font = Plots.font("Helvetica", 18)
 
   matplotlib.rcParams["text.usetex"] = true
   matplotlib.rcParams["text.latex.preamble"] = "\\usepackage{lmodern}"
