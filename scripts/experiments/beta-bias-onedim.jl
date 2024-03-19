fcecfdedcaf86440388ad62217c42ce4f9d068a2
Johan Larsson <johanlarsson@outlook.com>
2024-03-19 16:39:35 +0100

    feat(experiments): parametrize bias-var experiment using delta

--- scripts/experiments/beta-bias-onedim.jl
+++ scripts/experiments/beta-bias-onedim.jl
@@ -11,18 +11,12 @@ using Plots.PlotMeasures
 
 set_plot_defaults()
 
-function onedim_bias_sim(q::Real, σe::Real, method::String, n::Int64, λ::Real = 0.5)
+function onedim_bias_sim(q::Real, σe::Real, δ::Real, n::Int64, λ::Real = 0.5)
   Random.seed!(852)
 
   β = 1
 
-  if method == "var"
-    s = q * (1 - q)
-  elseif method == "std"
-    s = sqrt(q * (1 - q))
-  elseif method == "none"
-    s = 1
-  end
+  s = (q - q^2)^δ
 
   μ = binary_mean(β, n, q, s)
   σ = binary_stddev(σe, n, q, s)
@@ -32,19 +26,23 @@ function onedim_bias_sim(q::Real, σe::Real, method::String, n::Int64, λ::Real 
   θ = -(μ + λ)
   γ = μ - λ
 
-  eβ = binary_expected_value(θ, γ, σ, d, print_components = true)
-  2 * β * λ * pdf(Normal(), λ / (σe * sqrt(n))) / (σe * sqrt(n))
+  eβ = binary_expected_value(θ, γ, σ, d, print_components = false)
+  v = binary_variance(θ, γ, σ, d)
+
+  # std_level = 2 * β * λ * pdf(Normal(), λ / (σe * sqrt(n))) / (σe * sqrt(n))
+  std_level = 2 * β * cdf(Normal(), -β * sqrt(n) / σe)
+  
 
   bias = eβ - β
-  mse = bias^2 + var
+  mse = bias^2 + v
 
-  return bias, var, mse
+  return bias, v, mse
 end
 
 param_dict = Dict{String,Any}(
   "q" => collect(0.5:0.01:0.99),
   "sigma_e" => [0.1, 0.5, 1, 4],
-  "method" => ["std", "var", "none"],
+  "delta" => [0, 1/4, 1/2, 1, 2],
   "lambda" => [0.2],
 )
 param_expanded = dict_list(param_dict)
@@ -52,12 +50,12 @@ param_expanded = dict_list(param_dict)
 results = []
 
 for (i, d) in enumerate(param_expanded)
-  @unpack q, sigma_e, method, lambda = d
+  @unpack q, sigma_e, delta, lambda = d
 
   n = 100
   lambda = n * lambda
 
-  bias, var, mse = onedim_bias_sim(q, sigma_e, method, n, lambda)
+  bias, var, mse = onedim_bias_sim(q, sigma_e, delta, n, lambda)
 
   d_exp = copy(d)
   d_exp["bias"] = bias
@@ -68,14 +66,16 @@ for (i, d) in enumerate(param_expanded)
 end
 
 df = DataFrame(results);
-df_long = stack(df, Not([:q, :sigma_e, :method, :lambda]))
+df_long = stack(df, Not([:q, :sigma_e, :delta, :lambda]))
 
-n_rows = length(unique(df.method))
-n_cols = length(unique(df.sigma_e))
+n_delta = length(unique(df.delta))
+n_sigma = length(unique(df.sigma_e))
 
 grouped_df = groupby(df_long, [:variable])
 
 plots = []
+
+pal = :Johnson
 
 for (i, d) in enumerate(grouped_df)
   subgrouped_df = groupby(d, [:sigma_e])
@@ -107,28 +107,52 @@ for (i, d) in enumerate(grouped_df)
       x -> ""
     end
 
-    pl = @df dd plot(
+    pl = plot(xlims = (0.45, 1.05))
+
+    if i == 2
+      β = 1
+      n = 100
+      σe = unique(dd.sigma_e)[1]
+      λ = 0.2 * n
+      lev = 2 * β * cdf(Normal(), -λ / (σe * sqrt(n))) - β
+      println("lev:", lev)
+      hline!(pl, [lev], linestyle = :dot, linecolor = :black)
+    end
+
+    @df dd plot!(
+      pl,
       :q,
       :value,
-      groups = :method,
+      groups = :delta,
       ylabel = ylab,
       xlabel = xlab,
       xformatter = xformatter,
       xticks = 0.5:0.25:1,
       title = title,
       legend = false,
+      palette=pal,
     )
 
     push!(plots, pl)
   end
 end
 
-lab = reshape(sort(unique(df.method)), 1, 3)
+lab = reshape(sort(unique(df.delta)), 1, n_delta)
+
+legendvals = collect(zeros(n_delta)')
 
 legend =
-  plot([0 0 0], showaxis = false, grid = false, label = lab, legend_position = :topleft)
+  plot(
+    legendvals,
+    showaxis = false,
+    grid = false,
+    label = lab,
+    legend_position = :topleft,
+    legend_title = L"\delta",
+    palette=pal,
+  )
 
-l = @layout[grid(3, 4) a{0.15w}]
+l = @layout[grid(3, n_sigma) a{0.15w}]
 
 plotlist = plot(plots..., legend, layout = l, size = (575, 400))
 
