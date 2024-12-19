9f7ff7a38786da731f0b54c3e78aa6b7a5216d22
Johan Larsson <jolars@posteo.com>
2024-12-19 09:21:34 +0100

    chore: crop all plots

---
+++ utils/crop-plots.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Directory containing the PDF files
+PDF_DIR="paper/plots"
+
+# Loop through all PDF files in the directory
+for pdf_file in "$PDF_DIR"/*.pdf; do
+  # Crop the PDF file and overwrite the original file
+  pdfcrop "$pdf_file" "$pdf_file"
+done
